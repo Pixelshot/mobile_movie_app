5d3bf3732db0ac361c1cf46a5faa05947739934a
pixelshot <pixelshot8@gmail.com>
2025-05-16 11:52:54 +0800

    Fixed pathing issues and getting tab icons ready

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,7 +5,7 @@ import { icons } from '@/constants/icons'
 
 const TabIcon = () => {
   return (
-    <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-14mt-4 justify-center items=center rounded-full overflow-hidden ">
+    <ImageBackground source={images.highlight} className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden ">
       <Image source={icons.home} tintColor="#151312" className="size-5" />
       <Text className="text-secondary text-base font-semibold ml-2">Home</Text>
     </ImageBackground>
@@ -16,13 +16,33 @@ const _Layout = () => {
   return (
     <Tabs>
       <Tabs.Screen name="index" options={{
-        title: 'Home', headerShown: false, tabBarIcon: ({ focused }) => (
+        title: 'Home',
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
           <TabIcon />
         )
       }} />
-      <Tabs.Screen name="search" options={{ title: 'Search', headerShown: false }} />
-      <Tabs.Screen name="saved" options={{ title: 'Saved', headerShown: false }} />
-      <Tabs.Screen name="profile" options={{ title: 'Profile', headerShown: false }} />
+      <Tabs.Screen name="search" options={{
+        title: 'Search',
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon />
+        )
+      }} />
+      <Tabs.Screen name="saved" options={{
+        title: 'Saved',
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon />
+        )
+      }} />
+      <Tabs.Screen name="profile" options={{
+        title: 'Profile',
+        headerShown: false,
+        tabBarIcon: ({ focused }) => (
+          <TabIcon />
+        )
+      }} />
     </Tabs>
   )
 }
